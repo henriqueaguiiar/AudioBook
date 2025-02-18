d966b95a88e0d17d3e92616c6bec1d7ae66c9153
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-02-18 00:14:48 -0300

    feat: Created entities Superclass and subclass

--- src/com/henriqueaguiiar/entities/Audio.java
+++ src/com/henriqueaguiiar/entities/Audio.java
@@ -1,4 +1,38 @@
 package com.henriqueaguiiar.entities;
 
 public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private double classicacao;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public double getClassicacao() {
+        return classicacao;
+    }
+
+    public void curte(){
+        this.totalCurtidas++;
+    }
+    public void reproduzir(){
+        this.totalReproducoes++;
+    }
+
 }
+
